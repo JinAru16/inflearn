8188d51de35440411c0f505f0cde9f7798c31b2b
JinAru16 <wjdcksdnr11@naver.com>
2023-06-07 10:50:36 +0900

    http request parameter / query parameter, html form

---
+++ mvcbasicfunction/src/main/java/com/springmvc/mvcbasicfunction/request/RequestParamController.java
@@ -0,0 +1,26 @@
+package com.springmvc.mvcbasicfunction.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}, age={}", username, age);
+
+        response.getWriter().write("ok");
+    }
+    /*
+    http://loaclhost:8080/request-param-v1?username=kim&age=20 이런식으로 파라미터를 보내면됨.
+     */
+}
