14f5c63dba6ca8a54ef9bff8447c0c47471ad93e
JinAru16 <wjdcksdnr11@naver.com>
2023-06-16 14:06:04 +0900

    이사

--- cptPanGyo/vueMiddle/vse/vue-todo/src/components/TodoInput.vue
+++ cptPanGyo/vueMiddle/vse/vue-todo/src/components/TodoInput.vue
@@ -4,14 +4,24 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+    <Modal v-if="showModal" @close="showModal = false">
+        <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+      <h3 slot="header">custom header</h3>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue';
+
 export default {
   data: function() {
     return {
-      newTodoItem: ''
+      newTodoItem: '',
+      showModal: false
     }
   },
   methods: {
@@ -19,11 +29,16 @@ export default {
       if (this.newTodoItem !== '') {
         this.$emit('addTodoItem', this.newTodoItem);
         this.clearInput();
+      } else{
+        this.showModal = !this.showModal;
       }
     },
     clearInput: function() {
       this.newTodoItem = '';
     }
+  },
+  components: {
+    Modal: Modal
   }
 }
 </script>
