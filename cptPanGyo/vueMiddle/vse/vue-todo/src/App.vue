eb7433bb973f0c3f780b18f81948a6c35e25a75e
JinAru16 <wjdcksdnr11@naver.com>
2023-06-19 15:44:15 +0900

    commit, mutations

--- cptPanGyo/vueMiddle/vse/vue-todo/src/App.vue
+++ cptPanGyo/vueMiddle/vse/vue-todo/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <TodoHeader></TodoHeader>
     <!-- <TodoInput v-on:하위컴포넌트에서 발생시킨 이벤트 이름="현재 컴포넌트의 메소드 이름"></TodoInput> -->
-    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+    <TodoInput></TodoInput>
     <!--<TodoList v-bind:내려보낼 프롭스 속성 이름 = "현재 위치의 컴포넌트 데이터 속성"></TodoList> -->
     <TodoList v-bind:propsdata="todoItems" 
     v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></TodoList>
@@ -23,11 +23,7 @@ export default {
     }
   },
   methods:{
-    addOneItem (todoItem){
-    const obj = {completed: false, item: todoItem};
-        localStorage.setItem(todoItem, JSON.stringify(obj));
-        this.todoItems.push(obj);      
-    },
+
     removeOneItem (todoitem, index){
       localStorage.removeItem(todoitem.item);
       this.todoItems.splice(index, 1);
@@ -43,15 +39,7 @@ export default {
       this.todoItems = []
     }
   },
-  created() {
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-  },
+  
   components: {
     // 컴포넌트 태그명 : 컴포넌트 내용  현재 둘다 이름이 같은 상태
     // 'TodoHeader': TodoHeader, => TodoHeader 하나로 축약가능
--- cptPanGyo/vueMiddle/vse/vue-todo/src/store/store.js
+++ cptPanGyo/vueMiddle/vse/vue-todo/src/store/store.js
@@ -1,8 +1,36 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 
 Vue.use(Vuex);
+const storage = {
+  fetch() {
+    const arr = [];
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+    return arr;
+  },
+};
 
 export const store = new Vuex.Store({
-  
-})
+  state:{
+    headerText: 'Todo it from Store Using Vuex',
+    todoItems: storage.fetch()
+  },
+  getters:{
+    getTodoItems(state){
+      return state.todoItems;
+    }
+  },
+  mutations:{
+    addOneItem(state, todoItem) {
+      const obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      state.todoItems.push(obj); 
+    }
+  }
+});
