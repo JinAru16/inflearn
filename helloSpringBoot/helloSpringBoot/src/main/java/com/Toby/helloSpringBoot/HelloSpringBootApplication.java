96e2931b83fab3b2409ccb849187c06da09bc7bd
JinAru16 <wjdcksdnr11@naver.com>
2023-06-26 15:39:34 +0900

    trasition to front controller

--- helloSpringBoot/helloSpringBoot/src/main/java/com/Toby/helloSpringBoot/HelloSpringBootApplication.java
+++ helloSpringBoot/helloSpringBoot/src/main/java/com/Toby/helloSpringBoot/HelloSpringBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -26,14 +27,27 @@ public class HelloSpringBootApplication {
 	public static void main(String[] args) {
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("frontController", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+					// 인증, 보안, 다국어, 공통 기능을 처리했다 치고
+					//웹 요청이 들어오면 프론트 컨트롤러가 매핑해주는거임.
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+						String name = req.getParameter("name");
 
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello "+ name);
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello "+ name);
+					}
+					else if(req.getRequestURI().equals("/user")) {
+						//
+					}
+					else{
+						resp
+								.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+
+
 				}
 			}).addMapping("/hello");
 		});
