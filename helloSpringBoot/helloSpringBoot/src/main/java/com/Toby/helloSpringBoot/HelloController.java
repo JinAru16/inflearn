9242c3603f544b0f434c13c235f445465b249979
JinAru16 <wjdcksdnr11@naver.com>
2023-06-26 16:29:58 +0900

    add Service

--- helloSpringBoot/helloSpringBoot/src/main/java/com/Toby/helloSpringBoot/HelloController.java
+++ helloSpringBoot/helloSpringBoot/src/main/java/com/Toby/helloSpringBoot/HelloController.java
@@ -3,11 +3,14 @@ package com.Toby.helloSpringBoot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(String name){
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+        return helloService.sayHello(Objects.requireNonNull(name)); //Objects.requireNonNull : null이 아니면 객체를 반환하고, null이면 예외를 터트림
     }
 }
---
+++ helloSpringBoot/helloSpringBoot/src/main/java/com/Toby/helloSpringBoot/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package com.Toby.helloSpringBoot;
+
+public class SimpleHelloService {
+    String sayHello(String name){
+        return "Hello" + name;
+    }
+}
