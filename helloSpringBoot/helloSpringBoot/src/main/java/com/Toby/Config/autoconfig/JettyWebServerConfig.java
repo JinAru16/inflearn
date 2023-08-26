f0e6d62fb312c8bbcb392525b03b0dee890497a7
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2023-08-26 23:48:55 +0900

    custom@Conditional

--- helloSpringBoot/helloSpringBoot/src/main/java/com/Toby/Config/autoconfig/JettyWebServerConfig.java
+++ helloSpringBoot/helloSpringBoot/src/main/java/com/Toby/Config/autoconfig/JettyWebServerConfig.java
@@ -2,20 +2,21 @@ package com.Toby.Config.autoconfig;
 
 import com.Toby.Config.ConditionalMyOnClass;
 import com.Toby.Config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 @MyAutoConfiguration
-@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
-    public ServletWebServerFactory servletWebServerFactory(){
-        return new TomcatServletWebServerFactory();
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new JettyServletWebServerFactory();
     }
 }
