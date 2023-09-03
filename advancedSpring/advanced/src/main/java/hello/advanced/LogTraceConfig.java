4b3dfe5a92c4f7d037f330e7d887e942a40e8903
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2023-09-03 21:36:42 +0900

    Thread Local apply

--- advancedSpring/advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advancedSpring/advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -2,13 +2,20 @@ package hello.advanced;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogTraceConfig {
+//    @Bean
+//    public LogTrace logTrace(){
+//        return new FieldLogTrace();
+//    }
+// 쓰레드 적용한 LogTrace로 변경을 위한 주석
+
     @Bean
     public LogTrace logTrace(){
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
