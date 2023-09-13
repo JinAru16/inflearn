f2ba2cbae6c193bd25297376b73889ed4ff6192b
JinAru16 <wjdcksdnr11@naver.com>
2023-09-13 09:47:11 +0900

    interface based proxy apply

--- advancedSpring/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ advancedSpring/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,12 +2,17 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 /**
  * ScanBasePackages를 별로로 적용해주면 해당 패키지와 그 패키지 하위만 컴포넌트 스캔을 하게 됨.
@@ -21,6 +26,10 @@ public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
+	}
+	@Bean
+	public LogTrace logTrace(){
+		return new ThreadLocalLogTrace();
 	}
 
 }
--- advancedSpring/proxy-start/src/main/java/hello/proxy/config/v1_proxy/InterfaceProxyConfig.java
+++ advancedSpring/proxy-start/src/main/java/hello/proxy/config/v1_proxy/InterfaceProxyConfig.java
@@ -1,24 +1,36 @@
 package hello.proxy.config.v1_proxy;
 
 import hello.proxy.app.v1.*;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderControllerInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderRepositoryInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderServiceInterfaceProxy;
+import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InterfaceProxyConfig {
-
+/*
+proxy(프록시) -> target(구현체)
+orderServiceInterfaceProxy(프록시) -> orderServiceV1Impl(구현체)
+스프링빈으로 실제 객체 대신에 프록시 객체를 등록했기 때문에 앞으로 스프링 빈을 주입 받으면 실제 객체 대신에 프록시 객체가 주입된다.
+프록시 객체는 스프링 컨테이너가 관리하고 자바 힙 메모리에도 올라간다. 반면에 실제 객체는 자바 힙 메모리에는 올라가지만 스프링 컨테이너가 관리하지는 않는다.
+ */
     @Bean
-    public OrderControllerV1 orderController(){
-        return new OrderControllerV1Impl(orderService());
+    public OrderControllerV1 orderController(LogTrace logTrace){
+        OrderControllerV1Impl controller = new OrderControllerV1Impl(orderService(logTrace));
+        return new OrderControllerInterfaceProxy(controller, logTrace);
     }
 
     @Bean
-    public OrderServiceV1 orderService(){
-        return new OrderServiceV1Impl(orderRepository());
+    public OrderServiceV1 orderService(LogTrace logTrace){
+        OrderServiceV1Impl serviceImpl = new OrderServiceV1Impl(orderRepository(logTrace));
+        return new OrderServiceInterfaceProxy(serviceImpl, logTrace);
     }
 
     @Bean
-    public OrderRepositoryV1 orderRepository(){
-        return new OrderRepositoryV1Impl();
+    public OrderRepositoryV1 orderRepository(LogTrace logTrace){
+        OrderRepositoryV1Impl repositoryImpl = new OrderRepositoryV1Impl();
+        return new OrderRepositoryInterfaceProxy(repositoryImpl, logTrace);
     }
 }
