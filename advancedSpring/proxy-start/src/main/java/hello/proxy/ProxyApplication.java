8775369c770c950f39936fdc3b223c6cf9648aaf
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2023-10-10 22:08:12 +0900

    apply bean post process

--- advancedSpring/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ advancedSpring/proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -7,6 +7,7 @@ import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfig1V1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -20,7 +21,8 @@ import org.springframework.context.annotation.Import;
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(ProxyFactoryConfig1V1.class)
-@Import(ProxyFactoryConfigV2.class)
+//@Import(ProxyFactoryConfigV2.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 /**
  * ScanBasePackages를 별로로 적용해주면 해당 패키지와 그 패키지 하위만 컴포넌트 스캔을 하게 됨.
