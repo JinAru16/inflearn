47e70301922d285764063d068396f21bfb194eda
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2023-11-06 22:43:58 +0900

    jdbc insert

---
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,15 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.DriverManager;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager(){
+        //DriverManager.getConnection()
+    }
+}
