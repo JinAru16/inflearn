24af25445d2aebee785ecb32e5cfdfb7c7485493
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2023-12-04 00:25:08 +0900

    variable initialize

---
+++ java/java-start/src/variable/Var6.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        //System.out.println(a);// 초기화 하지 않고 선언만 한 변수에 대해서는 컴파일 에러가 발생. -> 메모리상에 이전에 어떤값이 있었는지 알길이 없기에 그대로 출력되면 메모리에 이전에 저장된 값이 출력 될 수 있기 때문에 초기화를 강제한다.
+    }
+}
