41a0c6a9e6ac798a559a54dc3df406dc3a6fe403
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2023-12-03 23:05:37 +0900

    about java

---
+++ java/java-start/src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java 1");// hello java 1을 출력합니다
+        //System.out.println("hello java 2");
+
+        /*
+        System.out.println("htest");
+        System.out.println("java 3");
+         */
+
+    }
+}
