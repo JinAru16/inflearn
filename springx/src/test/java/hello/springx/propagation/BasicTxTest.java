ee206e4ebf2fd9c8d4f574293345b439f696f9a3
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2024-01-08 22:22:35 +0900

    inner rollback

--- springx/src/test/java/hello/springx/propagation/BasicTxTest.java
+++ springx/src/test/java/hello/springx/propagation/BasicTxTest.java
@@ -93,4 +93,45 @@ public class BasicTxTest {
         log.info("외부 트랜잭션 커밋");
         txManager.commit(outer);
     }
+    @Test
+    void outer_rollback(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
+    }@Test
+    void inner_rollback(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner); // 내부적으로 외부에서 무슨일이 벌어지든지 롤백 무조건 시키라고 마킹을 함. 트랜잭션 동기화 매니저에 rollback = true 라고 표시를 해둠
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer); // 외부트랜잭션에서 커밋전에 트랜잭션 동기화 매니저를 한번 뒤져봄 뒤져보고 rollback = true라고 마킹되어 있으면 커밋이라고 되어있어도 그냥 롤백을 때려버림
+
+        /*
+        모든 논리 트랜잭션이 커밋이어야 실제로 커밋이 날아감. 내부든 외부든 하나라도 롤백이 있으면 전부 롤백시키는게 스프링 기본 정책임
+        근데 개발자는 커밋을 하라고 명령했는데 실제로는 롤백이 되었으니깐 예외를 터트려서 개발자에게 알려줌.
+        UnexpectedRollbackException을 터트려서 실제 의도와는 다르게 동작해서 어떻게 처리할지를 개발자에게 정하라고 알려줘야해서
+        예외를 터트리고 롤백을 시킴.
+
+        헷갈린다면 대원칙만 기억하자.
+        여러개의 논리 트랜잭션 중 하나라도 롤백이 발생하면 해당 물리 트랜잭션은 그냥 롤백이 된다고 생각하자.
+         */
+    }
 }
