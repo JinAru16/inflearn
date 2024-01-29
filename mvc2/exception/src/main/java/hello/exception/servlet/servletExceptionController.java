ba27b1a68aa799ab8ba2102f40bc873c3a65180c
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2024-01-30 00:18:06 +0900

    principle of working error page

---
+++ mvc2/exception/src/main/java/hello/exception/servlet/servletExceptionController.java
@@ -0,0 +1,29 @@
+package hello.exception.servlet;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class servletExceptionController {
+    @GetMapping("/error-ex")
+    public void errorEx(){
+        throw new RuntimeException("예외발생!");
+    }
+
+
+
+    @GetMapping("/error-404")
+    public void err404(HttpServletResponse response) throws IOException{
+        response.sendError(404, "404 오류!");
+    }
+
+    @GetMapping("/error-500")
+    public void err500(HttpServletResponse response) throws IOException{
+        response.sendError(500, "500 오류!");
+    }
+}
