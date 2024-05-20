61cec75e6207da8dd0e69e028252077eaf2eab4b
JinAru16 <wjdcksdnr11@naver.com>
2024-05-20 15:27:15 +0900

    wrapper class

---
+++ java-mid/src/lang/wrapper/WrapperClassMain.java
@@ -0,0 +1,9 @@
+package lang.wrapper;
+
+public class WrapperClassMain {
+    Integer newInteger = new Integer(10);// 미래에 삭제예정
+    Integer integerObj = Integer.valueOf(10); //-128 ~ 127 까지 자주 사용하는 숫자값은 자바가 미리 만들어두어 재사용, 불변
+
+    Long longObj = Long.valueOf(100);
+
+}
