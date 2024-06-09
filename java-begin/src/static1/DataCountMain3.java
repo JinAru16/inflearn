d530c21eee02b5660259576f082596a688e0705c
JinAru16 <wjdcksdnr11@naver.com>
2024-06-10 08:16:48 +0900

    멤버 변수의 종류, static변수 접근 방법

--- java-begin/src/static1/DataCountMain3.java
+++ java-begin/src/static1/DataCountMain3.java
@@ -11,6 +11,17 @@ public class DataCountMain3 {
         Data3 data3 = new Data3("C");
         System.out.println("C couont =" + Data3.count);
 
+        // 추가
+        // 인스턴스를 통한 접근 -> 인스턴스에 접근을 해봤더니 static이네? 다시 static 영역에 가서 불러오는 방식
+        /*
+        관례상 별로 권장하지 않음. 왜냐? 인스턴스 변수인지 static 변수인지 구별이 잘 안되기 때문.
+         */
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count);
+
+        //클래스를 통한 접근
+        System.out.println(Data3.count);
+
 
     }
 }
