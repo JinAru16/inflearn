51bdcacecc998fa690155ea2717d9f11b14aaa56
JinAru16 <wjdcksdnr11@naver.com>
2024-06-19 08:05:24 +0900

    aa

--- java-mid/src/Main.java
+++ java-mid/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] a = {0, 1};
+        int n = a.length;
+        int missing =0;
+        for(int i=0; i<n; i++){
+            int finalI = i;
+            if(Arrays.stream(a).anyMatch(value -> finalI == value)){
+                continue;
+            }else{
+                missing = i;
+                break;
+            }
+        }
+        if(missing == 0){
+            missing = n;
+        }
+
+        System.out.println(missing);
     }
 }
