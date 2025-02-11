1f8108537fdcd8aca033231ef40b8dcd6ff15ecc
Chanwook Jeong <wjdcksdnr11@naver.com>
2025-02-11 22:16:36 +0900

    x

--- advancedSpring/advanced/src/main/java/hello/advanced/app/v0/OrderControllerV0.java
+++ advancedSpring/advanced/src/main/java/hello/advanced/app/v0/OrderControllerV0.java
@@ -4,6 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.function.BiConsumer;
+
 @RestController
 @RequiredArgsConstructor
 public class OrderControllerV0 {
@@ -12,6 +17,19 @@ public class OrderControllerV0 {
     @GetMapping("/v0/request")
     public String request(String itemId){
         orderService.orderItem(itemId);
+
+
+
+
         return "ok";
     }
+    public int[][] solution(int[] num_list, int n) {
+        int size = num_list.length/n;
+        int[][] answer = new int[size][n];
+
+        answer[1][3] = 3;
+
+    }
+
+
 }
