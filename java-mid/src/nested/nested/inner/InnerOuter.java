fc5aed5a43c8748f5a0280d47382a14b0293df91
Chanwook Jeong <wjdcksdnr11@naver.com>
2025-02-12 21:59:56 +0900

    내부클래스 실습

---
+++ java-mid/src/nested/nested/inner/InnerOuter.java
@@ -0,0 +1,35 @@
+package nested.nested.inner;
+
+
+public class InnerOuter {
+    private static int outClassValue = 3;
+    private int outInstanceValue = 2;
+
+    class Inner {
+        private int innerInstanceValue = 1;
+
+        public void print(){
+            // 자기 자신에 접근
+            System.out.println(innerInstanceValue);
+
+            // 외부 클래스의 인스턴스 멤버에 접근 가능. private도 접근 가능.
+            System.out.println(outInstanceValue);
+
+            // 외부 클래스의 클래스 멤버에 접근 가능. private도 접근 가능.
+            System.out.println(outClassValue);
+
+        }
+
+        public int getInnerInstanceValue() {
+            return innerInstanceValue;
+        }
+    }
+
+    public static int getOutClassValue() {
+        return outClassValue;
+    }
+
+    public int getOutInstanceValue() {
+        return outInstanceValue;
+    }
+}
