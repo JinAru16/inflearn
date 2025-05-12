a927f43b61fcd54068cea50c8d6f843784e875ca
JinAru16 <wjdcksdnr11@naver.com>
2025-05-12 14:16:55 +0900

    제네릭이 필요한 이유

---
+++ java-mid2/src/generic/ex0/StringBox.java
@@ -0,0 +1,12 @@
+package generic.ex0;
+
+public class StringBox {
+    private String value;
+
+    public void set(String value) {
+        this.value = value;
+    }
+    public String get() {
+        return value;
+    }
+}
